1f5f24541818b1ad3b904e659240400e836a71e8
Michael <michael@talofagames.com>
2025-08-23 13:04:13 -0700

    God log file changes

--- src/save.c
+++ src/save.c
@@ -126,7 +126,7 @@ void save_char_obj( CHAR_DATA *ch )
     if (IS_IMMORTAL(ch) || ch->level >= LEVEL_IMMORTAL)
     {
 	fclose(fpReserve);
-	sprintf(strsave, "%s%s",GOD_DIR, capitalize(ch->name));
+	sprintf(strsave, "%s%s.gsf",GOD_DIR, capitalize(ch->name));
 	if ((fp = fopen(strsave,"w")) == NULL)
 	{
 	    bug("Save_char_obj: fopen",0);
