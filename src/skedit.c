c3adf5d852abbd74532c7f7e6df9a231d6484133
Michael <michael@talofagames.com>
2025-08-23 08:21:27 -0700

    Multiplatform class support

--- src/skedit.c
+++ src/skedit.c
@@ -32,11 +32,19 @@ void do_group_info( CHAR_DATA *ch, char *argument);
 void do_skedit(CHAR_DATA *ch ,char * argument);
 
 // macro definitions
+#ifdef WIN32
 #define CLASS_TEMP "../data/class/t"
 #define CLASS_DIR "../data/class/"
 #define GROUP_TEMP "../data/group/t"
 #define GROUP_DIR "../data/group/"
 #define GROUP_LIST "../data/group/group.lst"
+#else
+#define CLASS_TEMP "data/class/t"
+#define CLASS_DIR "data/class/"
+#define GROUP_TEMP "data/group/t"
+#define GROUP_DIR "data/group/"
+#define GROUP_LIST "data/group/group.lst"
+#endif
 
 int max_groups;
 
