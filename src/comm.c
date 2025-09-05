5a2ffc2b5a73469cd95e664982fd3436b35861f1
Michael <michael@talofagames.com>
2025-09-05 08:19:27 -0700

    Uptime is no longer reset during copyover

--- src/comm.c
+++ src/comm.c
@@ -379,7 +379,9 @@ int main( int argc, char **argv )
      */
     gettimeofday( &now_time, NULL );
     current_time 	= (time_t) now_time.tv_sec;
-    startup_time 	= current_time;
+    if (fCopyOver == FALSE) {
+			startup_time 	= current_time;
+    }
     last_reset_time 	= current_time;
     strcpy( str_boot_time, ctime( &current_time ) );
 
